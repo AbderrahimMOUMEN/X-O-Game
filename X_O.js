d59c4406727abff2886477b764a09ab5d00d88a3
AbderrahimMOUMEN <74512954+AbderrahimMOUMEN@users.noreply.github.com>
2021-02-18 20:16:20 +0100

    Add files via upload

---
+++ X_O.js
@@ -0,0 +1,298 @@
+var c=0;
+var s="";
+var s1="";
+function play1()
+            {
+                var d1=document.getElementById('d1');
+                if(c%2==0)
+                {
+                     d1.innerHTML="X";
+                     c++;
+                     d1.style.pointerEvents="none";
+                     d1.style.background='none';
+                     d1.style.fontSize='60px';
+                     d1.style.color='Red';
+                     d1.style.border='none';
+                     d1.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d1.style.transform='scale(2)';
+                     return;
+                }
+                
+                if(c%2!=0){
+                    d1.innerHTML="O";
+                    c++;
+                    d1.style.pointerEvents="none";
+                    d1.style.background='none';
+                     d1.style.fontSize='60px';
+                     d1.style.color='Green';
+                     d1.style.border='none';
+                     d1.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d1.style.transform='scale(2)';
+                    return;
+                }
+                
+            }
+function play2()
+            {
+                var d2=document.getElementById('d2');
+                
+                if(c%2==0)
+                {
+                    
+                     d2.innerHTML="X";
+                     c++;
+                     d2.style.pointerEvents="none";
+                     d2.style.background='none';
+                     d2.style.fontSize='60px';
+                     d2.style.color='Red';
+                     d2.style.border='none';
+                     d2.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d2.style.transform='scale(2)';
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    
+                    d2.innerHTML="O";
+                    c++;
+                    d2.style.pointerEvents="none";                   
+                     d2.style.fontSize='60px';
+                     d2.style.color='green';
+                     d2.style.border='none';
+                     d2.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d2.style.transform='scale(2)';
+                    return;
+                }
+            }
+function play3()
+            {
+                var d3=document.getElementById('d3');
+               
+                if(c%2==0)
+                {
+                     d3.innerHTML="X";
+                     c++;
+                     d3.style.pointerEvents="none";
+                     d3.style.background='none';
+                     d3.style.fontSize='60px';
+                     d3.style.color='Red';
+                     d3.style.border='none';
+                     d3.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d3.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    
+                    d3.innerHTML="O";
+                    c++;
+                    d3.style.pointerEvents="none";
+                    d3.style.background='none';
+                     d3.style.fontSize='60px';
+                     d3.style.color='green';
+                     d3.style.border='none';
+                     d3.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d3.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play4()
+            {
+                var d4=document.getElementById('d4');
+                if(c%2==0)
+                {
+                     d4.innerHTML="X";
+                     c++;
+                     d4.style.pointerEvents="none";
+                     d4.style.fontSize='60px';
+                     d4.style.color='Red';
+                     d4.style.border='none';
+                     d4.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d4.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d4.innerHTML="O";
+                    c++;
+                    d4.style.pointerEvents="none";
+                    
+                     d4.style.fontSize='60px';
+                     d4.style.color='green';
+                     d4.style.border='none';
+                     d4.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d4.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play5()
+            {
+                var d5=document.getElementById('d5');
+                if(c%2==0)
+                {
+                     d5.innerHTML="X";
+                     c++;
+                     d5.style.pointerEvents="none";
+                     d5.style.background='none';
+                     d5.style.fontSize='60px';
+                     d5.style.color='Red';
+                     d5.style.border='none';
+                     d5.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d5.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d5.innerHTML="O";
+                    c++;
+                    d5.style.pointerEvents="none";
+                    d5.style.background='none';
+                     d5.style.fontSize='60px';
+                     d5.style.color='green';
+                     d5.style.border='none';
+                     d5.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d5.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play6()
+            {
+                var d6=document.getElementById('d6');
+                if(c%2==0)
+                {
+                     d6.innerHTML="X";
+                     c++;
+                     d6.style.pointerEvents="none";
+                     d6.style.background='none';
+                     d6.style.fontSize='60px';
+                     d6.style.color='Red';
+                     d6.style.border='none';
+                     d6.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d6.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d6.innerHTML="O";
+                    c++;
+                    d6.style.pointerEvents="none";
+                    d6.style.background='none';
+                     d6.style.fontSize='60px';
+                     d6.style.color='green';
+                     d6.style.border='none';
+                     d6.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d6.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play7()
+            {
+                var d7=document.getElementById('d7');
+                if(c%2==0)
+                {
+                     d7.innerHTML="X";
+                     c++;
+                     d7.style.pointerEvents="none";
+                     d7.style.background='none';
+                     d7.style.fontSize='60px';
+                     d7.style.color='Red';
+                     d7.style.border='none';
+                     d7.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d7.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d7.innerHTML="O";
+                    c++;
+                    d7.style.pointerEvents="none";
+                    d7.style.background='none';
+                     d7.style.fontSize='60px';
+                     d7.style.color='green';
+                     d7.style.border='none';
+                     d7.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d7.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play8()
+            {
+                var d8=document.getElementById('d8');
+                if(c%2==0)
+                {
+                     d8.innerHTML="X";
+                     c++;
+                     d8.style.pointerEvents="none";
+                     d8.style.background='none';
+                     d8.style.fontSize='60px';
+                     d8.style.color='Red';
+                     d8.style.border='none';
+                     d8.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d8.style.transform='scale(2)';
+
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d8.innerHTML="O";
+                    c++;
+                    d8.style.pointerEvents="none";
+                    d8.style.background='none';
+                     d8.style.fontSize='60px';
+                     d8.style.color='green';
+                     d8.style.border='none';
+                     d8.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d8.style.transform='scale(2)';
+
+                    return;
+                }
+            }
+function play9()
+            {
+                var d9=document.getElementById('d9');
+                if(c%2==0)
+                {
+                     d9.innerHTML="X";
+                     c++;
+                     d9.style.pointerEvents="none";
+                     d9.style.background='none';
+                     d9.style.fontSize='60px';
+                     d9.style.color='Red';
+                     d9.style.border='none';
+                     d9.style.fontFamily=" 'Mr Bedfort', cursive";
+                     d9.style.transform='scale(2)';
+                     return;
+                     
+                }
+                if(c%2!=0){
+                    d9.innerHTML="O";
+                    c++;
+                    d9.style.pointerEvents="none";
+                    d9.style.background='none';
+                    d9.style.fontSize='60px';
+                    d9.style.color='green';
+                    d9.style.border='none';
+                    d9.style.fontFamily=" 'Mr Bedfort', cursive";
+                    d9.style.transform='scale(2)';
+
+                    return;
+                }
+            }   
+   
+   /* if((d1.value===d2.value && d2.value==d3.value) || (d4.value===d5.value && d5.value===d6.value)  )
+                {
+                   // test.innerHTML="You WOn";
+                }:*/
+    
+
+
